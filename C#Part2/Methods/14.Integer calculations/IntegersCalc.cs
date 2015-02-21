4f8de0345f01315be10e61cdc531e1762cbc70d8
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-21 17:11:19 +0200

    14.IntegersCalculation

---
+++ C#Part2/Methods/14.Integer calculations/IntegersCalc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+class IntegersCalc
+{
+
+    static int MinimumInteger(params int[] integers)
+    {
+        int min = integers[0];
+        for (int i = 0; i < integers.Length; i++)
+        {
+            if(min>integers[i])
+            {
+                min = integers[i];
+            }
+        }
+        return min;
+    }
+    static void Main()
+    {
+        /*
+         Problem 14. Integer calculations
+         Write methods to calculate minimum, maximum, average,
+         sum and product of given set of integer numbers.
+         Use variable number of arguments.
+         
+         */
+
+
+
+    }
+}
+
