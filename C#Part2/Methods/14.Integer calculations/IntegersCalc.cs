04bb2fe5c1de849c7ebbfec978702cf35adc521a
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-21 17:30:10 +0200

    14.Done

--- C#Part2/Methods/14.Integer calculations/IntegersCalc.cs
+++ C#Part2/Methods/14.Integer calculations/IntegersCalc.cs
@@ -16,6 +16,36 @@ class IntegersCalc
         }
         return min;
     }
+    static int MaximumInteger(params int [] integers)
+    {
+        int max = integers[0];
+        for (int i = 0; i < integers.Length; i++)
+        {
+            if(max<integers[i])
+            {
+                max = integers[i];
+            }
+        }
+        return max;
+    }
+    static long CalcSum(params int[] integers)
+    {
+        long sum = 0;
+        foreach (var item in integers)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+    static long Average(params int[] integrs)
+    {
+     
+        long sumNumber = CalcSum(integrs);
+
+       long  average = sumNumber / integrs.Length;
+       return average;
+      
+    }
     static void Main()
     {
         /*
@@ -26,6 +56,9 @@ class IntegersCalc
          
          */
 
+        Console.WriteLine(MinimumInteger(2, 3, 4, 5, 11, 2));
+        Console.WriteLine(MaximumInteger(4,6,8,99,0,6,45));
+        Console.WriteLine(Average(3, 65, 8, 9, 5, 3, 5, 5, 43, 3, 5, 7, 7));
 
 
     }
