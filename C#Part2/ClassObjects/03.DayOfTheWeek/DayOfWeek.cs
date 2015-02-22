0e93c436679b6105689fd16dee36a131a68d879d
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-22 20:39:44 +0200

    03.DayOfWeek

---
+++ C#Part2/ClassObjects/03.DayOfTheWeek/DayOfWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+class DayOfWeek
+{
+    static void Main()
+    {
+        /*
+         Problem 3. Day of week
+         Write a program that prints to the console which day of the week is today.
+         Use System.DateTime.
+         */
+
+        DateTime now = DateTime.Now;
+       string dayOfWeek=now.DayOfWeek.ToString();
+
+       Console.WriteLine(dayOfWeek);
+
+    }
+}
+
