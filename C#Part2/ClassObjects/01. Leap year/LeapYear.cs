2e06f7dfa171c9b960ab12f5839522478088cac3
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-22 20:24:40 +0200

    01.IsLeapYear

---
+++ C#Part2/ClassObjects/01. Leap year/LeapYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class LeapYear
+{
+    static void Main()
+    {
+        /*Problem 1. Leap year
+         Write a program that reads a year from the console and checks whether it is a leap one.
+         Use System.DateTime.
+         */
+
+        Console.Write("Etner a year Y = ");
+        int year =int.Parse(Console.ReadLine());
+      bool isLeapYear=DateTime.IsLeapYear(year);
+
+      Console.WriteLine("The year {0} is Leap = {1}", year, isLeapYear);
+
+    
+    }
+}
+
