8e7ad25eb24d4ca6ce1d8d2b7f7fa537814e7c42
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-22 20:32:49 +0200

    02.Random

---
+++ C#Part2/ClassObjects/02.Random numbers/Random.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class RandomNumber
+    {
+        static void Main()
+        {
+            /*Problem 2. Random numbers
+             Write a program that generates and prints to the console 10 random values in the range [100, 200].
+             
+             */
+
+
+            Random rand=new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int number = rand.Next(100, 200);
+
+                Console.WriteLine("{0} ", number);
+            }
+        }
+    }
+
