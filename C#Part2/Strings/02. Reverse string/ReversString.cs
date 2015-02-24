623a638f457a875d1da578570c7118958751b224
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-24 16:24:11 +0200

    02.ReversString

---
+++ C#Part2/Strings/02. Reverse string/ReversString.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.Reverse_string
+{
+    using System;
+    using System.Text;
+
+
+    class ReversString
+    {
+        static void ReverString(string string_text)
+        {
+            StringBuilder sb = new StringBuilder(string_text.Length);
+
+            for (int i = string_text.Length-1; i >= 0; i--)
+            {
+                sb.Append(string_text[i]);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+        static void Main()
+        {
+            /*
+             Problem 2. Reverse string
+             Write a program that reads a 
+             string, reverses it and prints the result at the console.
+             */
+
+            string text = "sample";
+
+            ReverString(text);
+        }
+    }
+}
