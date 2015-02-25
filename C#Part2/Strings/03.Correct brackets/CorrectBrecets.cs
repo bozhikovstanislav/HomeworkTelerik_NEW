58be2f6284c0ba34fccdb83890bacb2a6305dc5e
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-25 10:29:26 +0200

    Last

---
+++ C#Part2/Strings/03.Correct brackets/CorrectBrecets.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace _03.Correct_brackets
+{
+    using System;
+
+    class CorrectBrecets
+    {
+        static bool IsBracketsCorect(string expresion)
+        {
+            int countOpen = 0;
+            int countClosed = 0;
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                if( string.Compare("(",expresion[i].ToString())==0)
+                {
+                    countOpen++;
+                }
+                if (string.Compare(")", expresion[i].ToString()) == 0)
+                {
+                    countClosed++;
+                }
+            }
+            if(countOpen==countClosed)
+            {
+                return true;
+                
+            }
+            return false;
+        }
+
+        static void Main()
+        {
+            /*Problem 3. Correct brackets
+             
+             Write a program to check if in a given expression the brackets are put correctly.
+             Example of correct expression: ((a+b)/5-d). Example of incorrect expression: )(a+b)).
+             
+             */
+
+            string expresion="((a+b)/5-d)";
+           
+            bool isBrecetsCorect = IsBracketsCorect(expresion);
+            Console.WriteLine(isBrecetsCorect);
+          
+        }
+    }
+}
---
+++ C#Part2/Strings/04. Sub-string in text/SubstringInText.cs
@@ -0,0 +1,52 @@
+﻿namespace _04.Sub_string_in_text
+{
+    using System;
+    using System.Collections.Generic;
+
+
+    class SubstringInText
+    {
+
+        static int GetCountOfString(string string_text, string letter)
+        {
+            string textToLower = string_text.ToLower();
+            int index = string_text.IndexOf(letter);
+            int count = 0;
+            List<int> letterArray = new List<int>();
+            while (index!=-1)
+            {
+                index = string_text.IndexOf(letter, index + 1);
+                count++;
+            }
+            return count;
+        }
+
+
+
+
+        static void Main()
+        {
+            /*Problem 4. Sub-string in text
+             Write a program that finds how many times a sub-string is 
+             contained in a given text (perform case insensitive search).
+             Example:
+             
+             The target sub-string is in
+             The text is as follows:
+             We are living in an yellow submarine.
+             We don't have anything else. inside the submarine is very tight.
+             So we are drinking all the day. We will move out of it in 5 days.
+             
+             The result is: 9
+             
+             */
+            string str_text = @"We are living in an yellow submarine.
+             We don't have anything else. inside the submarine is very tight.
+             So we are drinking all the day. We will move out of it in 5 days.";
+
+            int countString = GetCountOfString(str_text, "in");
+            Console.WriteLine(countString);
+
+        }
+    }
+}
