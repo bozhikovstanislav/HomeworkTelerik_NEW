7669441b65ff34c7d972628c7e83588fda6b1cb6
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-26 12:08:39 +0200

    06

---
+++ C#Part2/Strings/06.Problem 6. String length/StringLenght.cs
@@ -0,0 +1,39 @@
+﻿namespace _06.Problem_6.String_length
+{
+    using System;
+
+
+    class StringLenght
+    {
+        static string FillAString(string text,int textLenght)
+        {
+
+            string textStr =string.Empty;
+
+            if(textStr.Length<textLenght)
+            {
+               textStr= text.PadRight(textLenght - textStr.Length, '*');
+            }
+
+            return textStr;
+
+        }
+
+        static void Main()
+        {
+            /*Problem 6. String length
+             
+             Write a program that reads from the console a string of maximum 20 characters.
+             If the length of the string is less than 
+             Print the result string into the console.
+             */
+
+
+            string text = "Thi";
+
+            int lenght = 20;
+
+            Console.WriteLine(FillAString(text,lenght));
+        }
+    }
+}
