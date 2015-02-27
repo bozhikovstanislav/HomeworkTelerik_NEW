7beb07ba6c2e34d897e0678f118fe745351dfc83
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-27 19:46:31 +0200

    08.Extra

---
+++ C#Part2/Strings/08. Extract sentences/ExtractSentance.cs
@@ -0,0 +1,60 @@
+﻿namespace _08.Extract_sentences
+{
+    using System;
+    using System.Collections.Generic;
+
+
+    class ExtractSentance
+    {
+        static int GetCountOfString(string string_text, string letter)
+        {
+            string textToLower = string_text.ToLower();
+            string tempLeter = " "+letter+" ";
+            int index = string_text.IndexOf(tempLeter);
+            int count = 0;
+            List<int> letterArray = new List<int>();
+            while (index != -1)
+            {
+                index = string_text.IndexOf(tempLeter, index + 1);
+
+                count++;
+            }
+            return count;
+        }
+        static void FindWordInSentance(string text,string word)
+        {
+            string[] splitText = text.Split('.');
+           for (int i = 0; i < splitText.Length; i++)
+			{
+                int countword = GetCountOfString(splitText[i], word);
+               if(countword>0)
+               {
+                   Console.WriteLine(splitText[i]);
+                   countword = 0;
+               }
+               else
+               {
+                   continue;
+               }
+			}
+           
+           
+        }
+
+        static void Main()
+        {
+            /*
+             Problem 8. Extract sentences
+             Write a program that extracts from a given text all sentences containing given word.
+             
+             */
+
+
+            string text = "We are living in a yellow submarine. We don't have anything else."+
+              " Inside the submarine is very tight. So we are drinking all the day."+
+              " We will move out of it in 5 days.";
+
+           FindWordInSentance(text,"in");
+        }
+    }
+}
