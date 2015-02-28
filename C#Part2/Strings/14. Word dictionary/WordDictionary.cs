c9c652fbf0a474b0324a48af5adb45deb0c138d6
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-28 14:46:59 +0200

    14Dictiinary

---
+++ C#Part2/Strings/14. Word dictionary/WordDictionary.cs
@@ -0,0 +1,46 @@
+﻿namespace _14.Word_dictionary
+{
+    using System;
+    using System.Collections.Generic;
+
+    class WordDictionary
+    {
+        static void DictionaryWord(string word)
+        {
+            Dictionary<string, string> wordDict = new Dictionary<string, string>();
+
+            wordDict.Add(".NET", "platform for applications from Microsoft");
+            wordDict.Add("CLR", "managed execution environment for .NET");
+            wordDict.Add("namespace", "hierarchical organization of classes");
+
+            
+                if(wordDict.ContainsKey(word))
+                {
+                    string value=wordDict[word];
+                    Console.WriteLine(value);
+                }
+                else
+                {
+                    Console.WriteLine("There is NO info about {0}", word);
+                }
+            
+
+        }
+
+        static void Main()
+        {
+            /*Problem 14. Word dictionary
+             A dictionary is stored as a sequence of text lines containing words and their explanations.
+             Write a program that enters a word and translates it by using the dictionary.
+             */
+
+            Console.Write("Enter a word = ");
+            string word = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("The info about the searched word is ;");
+            Console.WriteLine("-----------------------------------------");
+            DictionaryWord(word);
+            Console.WriteLine("_____________________________________________");
+        }
+    }
+}
