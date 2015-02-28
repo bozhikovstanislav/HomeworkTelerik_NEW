b7431019cb092457cfe3a1618e48465f6ddd42ad
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-02-28 19:11:16 +0200

    21

---
+++ C#Part2/Strings/21.Letters count/LetterCount.cs
@@ -0,0 +1,20 @@
+﻿namespace _21.Letters_count
+{
+    using System;
+    class LetterCount
+    {
+
+
+        static void Main()
+        {
+            /*Problem 21. Letters count
+             
+             Write a program that reads a string from the console and prints all different
+             letters in the string along with information how many times each letter is found.
+             
+             */
+
+
+        }
+    }
+}
