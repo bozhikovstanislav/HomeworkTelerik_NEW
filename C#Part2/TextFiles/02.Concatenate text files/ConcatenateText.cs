5aa1a2ff0eefbc3d1d269c20478a714811f67a12
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-03-02 16:33:29 +0200

    02.03LineNumbers

---
+++ C#Part2/TextFiles/02.Concatenate text files/ConcatenateText.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.Concatenate_text_files
+{
+    using System;
+    using System.IO;
+    class ConcatenateText
+    {
+        static void ConcatFiles(string pathFileOne,string pathFileTwo)
+        {
+            StreamReader readOne = new StreamReader(pathFileOne);
+            StreamReader readTwo = new StreamReader(pathFileTwo);
+
+
+            using (var output = File.Create(@"..\..\Result.txt",5,FileOptions.WriteThrough))
+            {
+                foreach (var file in new[] { pathFileOne, pathFileTwo })
+                {
+                    using (var input = File.OpenRead(file))
+                    {
+                        input.CopyTo(output);
+                    }
+                }
+            }
+
+           
+          
+            
+
+        }
+
+        static void Main()
+        {
+            /*Problem 2. Concatenate text files
+             
+             Write a program that concatenates two text files into another text file.
+             */
+            ConcatFiles(@"..\..\FileOne.txt", @"..\..\FileTwo.txt");
+
+        }
+    }
+}
---
+++ C#Part2/TextFiles/03.Line numbers/LineNubers.cs
@@ -0,0 +1,43 @@
+﻿namespace _03.Line_numbers
+{
+    using System;
+    using System.IO;
+
+
+    class LineNubers
+    {
+
+        static void readFile(string path)
+        {
+            StreamWriter writer = new StreamWriter(@"..\..\Result.txt");
+            using (writer)
+            {
+                StreamReader readerR = new StreamReader(path);
+                using (readerR)
+                {
+                    int lineNumber = 0;
+                    string line = readerR.ReadLine();
+                    while (line != null)
+                    {
+                        lineNumber++;
+                        Console.WriteLine("Line {0}: {1}",
+                        lineNumber, line);
+                        line = readerR.ReadLine();
+                        writer.WriteLine("Line {0}: {1}", lineNumber,line);
+                    }
+
+                }
+            }
+
+        }
+        static void Main()
+        {
+            /*Problem 3. Line numbers
+             Write a program that reads a text file and inserts line numbers in front of each of its lines.
+             The result should be written to another text file.
+             */
+
+            readFile(@"..\..\File.txt");
+        }
+    }
+}
