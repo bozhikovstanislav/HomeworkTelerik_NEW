922654787932cfe9913acaaefca4b8b2a2c2c30f
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-03-03 10:09:09 +0200

    08.ChangeName

--- C#Part2/TextFiles/08.Replace whole word/ReplaceWholeWord.cs
+++ C#Part2/TextFiles/08.Replace whole word/ReplaceWholeWord.cs
@@ -66,12 +66,12 @@
     }
         static void Main()
         {
-            /*
-             Problem 8. Replace whole word
-             Modify the solution of the previous problem to replace 
-             only whole words (not strings).
+            /*Problem 7. Replace sub-string
+           Write a program that replaces all occurrences of the
+           sub-string start with the sub-string finish in a text file.
+           Ensure it will work with large files (e.g. 100 MB).
              
-             */
+           */
             ReplaceStartFinish(@"..\..\File.txt");
         }
     }
