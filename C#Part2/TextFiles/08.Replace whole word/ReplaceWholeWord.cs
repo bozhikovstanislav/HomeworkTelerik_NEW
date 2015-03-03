b914b85fa3fe064e87aec63594c90b79af908646
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-03-03 10:05:47 +0200

    08.ReplaceWord

---
+++ C#Part2/TextFiles/08.Replace whole word/ReplaceWholeWord.cs
@@ -0,0 +1,78 @@
+﻿namespace _08.Replace_whole_word
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class ReplaceWholeWord
+    { static int readLinese(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            int lineNumber = 0;
+            using (reader)
+            {
+                
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    lineNumber++;
+                  
+                 
+                    line = reader.ReadLine();
+
+                }
+
+            }
+            return lineNumber;
+        }
+    static void ReplaceStartFinish(string path)
+    {
+
+
+        int lines = readLinese(path);
+        StreamReader reader = new StreamReader(path);
+        List<string> strBuilder = new List<string>();
+
+        using (reader)
+        {
+            string line = reader.ReadLine();
+
+            string result = line.Substring(line.IndexOf("Start")).Replace("Start", "finished");
+            strBuilder.Add(result);
+            while (line != null)
+            {
+                line = reader.ReadLine();
+                if (line == null)
+                {
+                    continue;
+                }
+                result = line.Substring(line.IndexOf("Start")).Replace("Start", "finished");
+                strBuilder.Add(result);
+            }
+        }
+
+
+
+        StreamWriter writer = new StreamWriter(path);
+        using (writer)
+        {
+
+            for (int i = 0; i < strBuilder.Count; i++)
+            {
+                writer.WriteLine(strBuilder[i]);
+
+            }
+        }
+    }
+        static void Main()
+        {
+            /*
+             Problem 8. Replace whole word
+             Modify the solution of the previous problem to replace 
+             only whole words (not strings).
+             
+             */
+            ReplaceStartFinish(@"..\..\File.txt");
+        }
+    }
+}
