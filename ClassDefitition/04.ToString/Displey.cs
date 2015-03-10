1509489e8a37f502d4e6c8d26d199cbc1f97feba
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-03-10 11:52:25 +0200

    02. constructors

---
+++ ClassDefitition/04.ToString/Displey.cs
@@ -0,0 +1,28 @@
+﻿namespace _04.ToString
+{
+   
+    using System;
+
+    enum BatteryTipe
+    {
+        LiIon, NiMH, NiCd
+    }
+    public class Displey
+    {
+        public uint? SizeDisplay { get; set; }
+        public uint? NuberOfColors { get; set; }
+        public Enum BatteryType { get; set; }
+
+
+        public Displey(uint? sizeDispley,uint? numbrOfColors,Enum batteryType)
+        {
+            this.SizeDisplay = sizeDispley;
+            this.NuberOfColors = numbrOfColors;
+            this.BatteryType = batteryType;
+        }
+        public Displey()
+        {
+
+        }
+    }
+}
---
+++ ClassDefitition/04.ToString/ToString.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.ToString
+{
+    using System;
+
+
+    class ToString
+    {
+
+        static void Main()
+        {
+            GSM gsmNokia = new GSM();
+            gsmNokia.BatteryGSM = new Battery();
+            gsmNokia.DispleyGSM = new Displey();
+            gsmNokia.BatteryGSM.HouerIdle = 123;
+            gsmNokia.BatteryGSM.HouerTalk = 4;
+            gsmNokia.Manifacturer = "Sout Korea";
+            gsmNokia.Model = "Samsung";
+            gsmNokia.Owner = "Gosho";
+            gsmNokia.Price = 124;
+            gsmNokia.BatteryGSM.ModelBatery = "Highe Cuality Battery";
+            gsmNokia.DispleyGSM.NuberOfColors = 245;
+            gsmNokia.DispleyGSM.SizeDisplay = 10;
+            gsmNokia.ToString();
+
+        }
+    }
+}
